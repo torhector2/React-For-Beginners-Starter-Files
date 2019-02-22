84752f8be7063f457f37008e7d5872cc0417849a
Hector Rodriguez Fornies <torhector2@gmail.com>
2019-02-22 11:55:15 +0700

    Move API keys to a config file and add it to gitignore

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,14 +1,7 @@
 import Rebase from "re-base"
 import firebase from "firebase"
+import config from './config'
 
-var config = {
-    apiKey: "AIzaSyBPlNuOSq5k087iMGP7W2j2S87uwsl5L8Y",
-    authDomain: "catch-of-the-day-wes-bos-ed7c1.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-wes-bos-ed7c1.firebaseio.com",
-    projectId: "catch-of-the-day-wes-bos-ed7c1",
-    storageBucket: "catch-of-the-day-wes-bos-ed7c1.appspot.com",
-    messagingSenderId: "996381071813"
-}
 const firebaseApp = firebase.initializeApp(config);
 
 const base = Rebase.createClass(firebaseApp.database())
