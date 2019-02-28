bdca2e47e7d677db41bd73109189094554cd5057
Hector Rodriguez Fornies <torhector2@gmail.com>
2019-02-28 10:38:32 +0700

    e23 component validation with PropTypes

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -1,15 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class EditFishForm extends React.Component {
-    handleChange = (event) => {
-        console.log(event.currentTarget.value)
-        //1. take a copy of the current fish
-        const updatedFish = { 
-            ...this.props.fish,
-            [event.currentTarget.name]: event.currentTarget.value
-        }
-      this.props.updateFish(this.props.index, updatedFish)
-    }
+  static propTypes = {
+    fish: PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      desc: PropTypes.string,
+      status: PropTypes.string,
+      price: PropTypes.number
+    }),
+    index: PropTypes.string,
+    updateFish: PropTypes.func
+  }
+  handleChange = event => {
+    console.log(event.currentTarget.value);
+    //1. take a copy of the current fish
+    const updatedFish = {
+      ...this.props.fish,
+      [event.currentTarget.name]: event.currentTarget.value
+    };
+    this.props.updateFish(this.props.index, updatedFish);
+  };
   render() {
     return (
       <div className="fish-edit">
@@ -45,9 +57,13 @@ class EditFishForm extends React.Component {
           onChange={this.handleChange}
           value={this.props.fish.image}
         />
-        <button onClick={() => {
-          this.props.deleteFish(this.props.index)
-        }}>Remove Fish</button>
+        <button
+          onClick={() => {
+            this.props.deleteFish(this.props.index);
+          }}
+        >
+          Remove Fish
+        </button>
       </div>
     );
   }
